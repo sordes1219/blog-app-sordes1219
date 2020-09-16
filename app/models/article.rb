5a357a2fd3545eb79540d32441540e86947dc0f9
romak <dieci.drago@gmail.com>
2020-09-17 00:49:37 +0900

    deviseによりユーザ認証機能を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,15 +13,16 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!\@)/}
+  validates :title, format: { with: /\A(?!\@)/ }
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
   validates :content, uniqueness: true
   validate :validate_title_and_content_length
 
   private
+
   def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
+    char_count = title.length + content.length
     errors.add(:content, '100文字以上にしてください') unless char_count > 100
   end
 end
