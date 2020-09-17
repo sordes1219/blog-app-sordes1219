b5cb88f20af47772dccd07b98cace6f867b9a7af
romak <dieci.drago@gmail.com>
2020-09-17 23:13:41 +0900

    自分の記事のみ編集可能にした

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_153533) do
+ActiveRecord::Schema.define(version: 2020_09_17_054117) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
