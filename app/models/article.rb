7e5d2100a06ea2a00e15d0fafae1e52273c4391b
romak <dieci.drago@gmail.com>
2020-09-30 14:44:18 +0900

    ActionTextの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,6 @@
 # Table name: articles
 #
 #  id         :bigint           not null, primary key
-#  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -16,20 +15,19 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!\@)/ }
-  validates :content, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :content, uniqueness: true
-  validate :validate_title_and_content_length
-
+  
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_one_attached :eyecatch
+  has_rich_text :content
+  
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!\@)/ }
+  validates :content, presence: true
 
-  def author_name
+    def author_name
     user.display_name
   end
 
@@ -37,10 +35,4 @@ class Article < ApplicationRecord
     likes.count
   end
 
-  private
-
-  def validate_title_and_content_length
-    char_count = title.length + content.length
-    errors.add(:content, '100文字以上にしてください') unless char_count > 100
-  end
 end
