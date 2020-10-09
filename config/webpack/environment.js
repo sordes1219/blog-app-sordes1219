61cb1b27a5d4ec522ee3d6f60d11daf8d33d9d66
romak <dieci.drago@gmail.com>
2020-10-10 07:46:52 +0900

    Revert "3つ目の解決策"
    This reverts commit bb8d2a493054b88911a6cb68c3f29023e40165cd.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,4 @@
 const { environment } = require('@rails/webpacker')
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Rails: '@rails/ujs'
-  })
-)
+Rails: ['@rails/ujs']
 
 module.exports = environment
