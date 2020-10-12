7555099b2701cf9801f0639cc53533ad877fe6d9
romak <dieci.drago@gmail.com>
2020-10-12 15:02:02 +0900

    デバック

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -2,6 +2,7 @@ class RelationshipMailer < ApplicationMailer
   def new_follower(user, follower)
     @user = user
     @follower = follower
+    puts account_url(@follower)
     mail to: user.email, subject: '【お知らせ】フォローされました'
   end
 end
